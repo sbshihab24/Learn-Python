1f4624e8d955d04b8c786d7a2b004db8ad7bf90e
sbshihab24 <sbshihab2000@gmail.com>
2025-09-02 06:13:16 +0600

    add some code

---
+++ practice.py
@@ -0,0 +1,16 @@
+print("Hello World !")       #print Hello World
+
+"""
+print("Mehedi Hasan Shihab")
+"""
+
+a=20
+b=30
+sum =a+b
+sub= b-a
+mul=a*b
+div=b/a
+print(sum)
+print(sub)
+print(mul)
+print(div)
