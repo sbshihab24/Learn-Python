76161750b78d0ade8fe9584aeff7c7abe1676770
sbshihab24 <sbshihab2000@example.com>
2026-04-06 00:30:48 +0600

    practice loops

---
+++ continue.py
@@ -0,0 +1,22 @@
+# for i in range(10):
+     
+#     if i==5:
+#         continue
+#     print(i)
+
+
+
+# a=0
+# while a<=10:
+#     a=a+1
+#     if a==5:
+#         continue
+#     print(a)
+
+
+
+#infinite loop
+
+a=10
+while a==10:
+    print(a)
---
+++ while_loops.py
@@ -0,0 +1,12 @@
+#while loop er moddhe kaj korte hoy 
+a=0
+while a<=10:
+    print(a)
+    a+=1
+#Break statement
+
+for i in range(10):
+     
+    if i==5:
+        break
+    print(i)
